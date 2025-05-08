89a06b4c930b5189850939fbab8bbead27921e2b
Divij Kapoor <divijkapoor015@gmail.com>
2025-05-07 17:01:02 -0700

    added user creation, login and logout

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.autocomplete, name="home"),
     path("autocomplete/", views.autocomplete_address, name="autocomplete"),
-    path("test/", views.test, name="test")
+    path("test/", views.test, name="test"),
+    path("users/", include("users.urls"))
 ]
